00cc2002e24d8929b6e72b3f4184bb4cc8f8edc9
Craig Cavanaugh <jgnash.devel@gmail.com>
2018-10-13 07:02:53 -0400

    WIP for PDF table generation using PDFBox.
    A nice result can be had with OpenPDF, but it lacks the facilities to
    create raster images for the report viewer.

---
+++ jgnash-report-core/src/main/java/jgnash/report/pdf/Report.java
@@ -0,0 +1,231 @@
+/*
+ * jGnash, a personal finance application
+ * Copyright (C) 2001-2018 Craig Cavanaugh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jgnash.report.pdf;
+
+import jgnash.report.table.AbstractReportTableModel;
+import jgnash.util.NotNull;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.util.Matrix;
+
+import java.io.IOException;
+
+import static jgnash.util.LogUtil.logSevere;
+
+/**
+ * Base report format definition.
+ *
+ * Units of measure is in Points
+ *
+ * @author Craig Cavanaugh
+ *
+ * TODO: Footer, full margin control
+ */
+public class Report {
+
+    private PDRectangle pageSize;
+
+    private float tableFontSize;
+
+    private PDFont tableFont;
+
+    private PDFont headerFont;
+
+    private boolean landscape = false;
+
+    private float margin;
+
+    private float cellPadding = 2;
+
+    public Report() {
+        pageSize = PDRectangle.LETTER;
+        tableFont = PDType1Font.HELVETICA;
+        tableFontSize = 12;
+    }
+
+    @NotNull
+    public PDRectangle getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(@NotNull PDRectangle pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    @NotNull
+    public PDFont getTableFont() {
+        return tableFont;
+    }
+
+    public void setTableFont(@NotNull PDFont tableFont) {
+        this.tableFont = tableFont;
+    }
+
+    public float getTableFontSize() {
+        return tableFontSize;
+    }
+
+    public void setTableFontSize(float tableFontSize) {
+        this.tableFontSize = tableFontSize;
+    }
+
+    private float getTableRowHeight() {
+        return getTableFontSize() + 2 * getCellPadding();
+    }
+
+    public boolean isLandscape() {
+        return landscape;
+    }
+
+    public void setLandscape(boolean landscape) {
+        this.landscape = landscape;
+    }
+
+    public PDFont getHeaderFont() {
+        return headerFont;
+    }
+
+    public void setHeaderFont(PDFont headerFont) {
+        this.headerFont = headerFont;
+    }
+
+    public float getCellPadding() {
+        return cellPadding;
+    }
+
+    public void setCellPadding(float cellPadding) {
+        this.cellPadding = cellPadding;
+    }
+
+    private float getAvailableHeight() {
+
+        if (isLandscape()) {
+            return getPageSize().getWidth() - getMargin() * 2;
+        }
+
+        return getPageSize().getHeight() - getMargin() * 2;
+    }
+
+    public void addTable(final PDDocument doc, final AbstractReportTableModel table) {
+
+        // TODO Allow for summation row for certain report types
+        final int rowsPerPage = (int)Math.floor(getAvailableHeight() / getTableRowHeight()) - 1;
+        final int numberOfPages = (int)Math.ceil((float)table.getRowCount()/ (float)rowsPerPage);
+
+        int startRow = 0;
+        int remainingRowCount = table.getRowCount();
+        int rows;
+
+        for (int i = 0; i < numberOfPages; i++) {
+            final PDPage page = createPage();
+            doc.addPage(page);
+
+            try (final PDPageContentStream contentStream = new PDPageContentStream(doc, page)) {
+                if (isLandscape()) {
+                   contentStream.transform(Matrix.getRotateInstance(Math.toRadians(90), 0, 0));
+                }
+
+                rows = rowsPerPage;
+
+                if (remainingRowCount < rows) {
+                    rows = remainingRowCount;
+                }
+
+                addTableSection(table, contentStream, startRow, rows);
+
+                remainingRowCount = remainingRowCount - rows;
+                startRow += rowsPerPage;
+
+            } catch (final IOException e) {
+               logSevere(Report.class, e);
+            }
+        }
+    }
+
+    private void addTableSection(final AbstractReportTableModel table, final PDPageContentStream contentStream,
+                                 final int startRow, final int rows) throws IOException {
+
+        float yPos = isLandscape() ? getPageSize().getWidth() - getMargin() : getPageSize().getHeight() - getMargin();
+        float xPos = getMargin() + getCellPadding();
+
+        yPos = yPos - (getTableRowHeight() / 2)
+                - ((getTableFont().getFontDescriptor().getFontBoundingBox().getHeight() / 1000 * getTableFontSize()) / 4);
+
+
+        contentStream.setFont(getHeaderFont(), getTableFontSize());
+
+
+        int columnWidth = 58;  // TODO, calculate/pack column widths
+
+        // draw header
+        for (int i = 0; i < table.getColumnCount(); i++) {
+            contentStream.beginText();
+            contentStream.newLineAtOffset(xPos, yPos);
+            contentStream.showText(table.getColumnName(i));
+            contentStream.endText();
+
+            xPos += columnWidth;
+        }
+
+        // draw rows
+        contentStream.setFont(getTableFont(), getTableFontSize());
+
+        for (int i = 0; i < rows; i++) {
+
+            int row = i + startRow;
+            xPos = getMargin() + getCellPadding();
+            yPos -= getTableRowHeight();
+
+            for (int j = 0; j < table.getColumnCount(); j++) {
+
+                final Object value = table.getValueAt(row, j);
+
+                if (value != null) {
+                    contentStream.beginText();
+                    contentStream.newLineAtOffset(xPos, yPos);
+                    contentStream.showText(table.getValueAt(row, j).toString());
+                    contentStream.endText();
+                }
+
+                xPos += columnWidth;
+            }
+        }
+
+        contentStream.close();
+    }
+
+
+    public float getMargin() {
+        return margin;
+    }
+
+    public void setMargin(float margin) {
+        this.margin = margin;
+    }
+
+    private PDPage createPage() {
+        PDPage page = new PDPage();
+        page.setMediaBox(getPageSize());
+        page.setRotation(isLandscape() ? 90 : 0);
+        return page;
+    }
+}
